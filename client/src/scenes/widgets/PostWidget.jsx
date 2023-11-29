3499b61372a52586a2e588f3b28ece7afc69e3ef
Arjun34-bit <arjundevendra34@gmail.com>
2023-11-29 20:00:49 +0530

    public

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -68,7 +68,7 @@ const PostWidget = ({
           height={"auto"}
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`/assets/${picturePath}`}
+          src={`/public/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
